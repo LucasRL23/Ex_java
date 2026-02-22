90cdc30ee1ce60af10f80622e54dd6daa8404889
Lucas Luciano <lucasrluciano23@gmail.com>
2026-02-22 20:08:37 -0300

    feat: logica ex 2

--- src/main/java/br/edu/ifsp/list01/Ex02.java
+++ src/main/java/br/edu/ifsp/list01/Ex02.java
@@ -21,14 +21,17 @@ import java.util.Scanner;
 public class Ex02 {
 
     public static void main(String[] args) {
-        //Leia o input
-        //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
-        //Escreva o resultado da chamada do método compute() aqui
+//        final Scanner scanner = new Scanner(System.in);
+//        final int input = scanner.nextInt();
+//        final Ex02 ex02 = new Ex01();
+//        System.out.println(ex01.compute(input));
     }
 
     String compute(int a, int b, int c) {
-        String output = null;
-        //put your logic here
-        return output;
+        if (a <= 0 || b <= 0 || c <= 0) return "Erro";
+        if (a == b && b ==c) return "Equilatero";
+        if (a ==b || b == c || a == c) return "Isosceles";
+        if (a + b <= c || b + c <= a || a + c <= b) return "Nao forma triangulo";
+        return "Escaleno";
     }
 }
